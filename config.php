ff5af3def71b6a7ab49c0b181e9bab4e222a05c4
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-13 05:51:53 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+// parameters for database login
+$host = "localhost";
+$username = "user";
+$password = " 123456";
+$dbname = "user";
+$conn = mysqli_connect($host,$username,$password,$dbname);
+if(!$conn){die("failed connection ". mysqli_connect_errno());
+}
+
+?>
