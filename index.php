657fc9f1c4b555457c2dc54764efe127f8ccb2cb
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-18 14:00:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php 
 // include the configuration file
-include confg.php;?>
+include "confg.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Create Accohnt</title>
+<meta name="description" content="create account">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="auth.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div id="container">
+<div class="topnav">
+<div id="mytopnav">
+  <a href="#">About</a>
+  <a href="#">Contact</a>
+   <a href="/logout">Log Out</a>
+ </div>
+<a href="javascript:void(0);" class="icon" onclick="displayIcon()"><i class="fa fa-bars"></i></a>
+</div>
+<div class="row">
+ <div class="col-12">
+<h2>Home Page</h2>
+</div>
+<div class="footer">
+&copy; copyright  ABC limited
+</div>
+<script src="landingpage.js"></script>
+</div>
+</body>
+</html>
