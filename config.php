a101e4baffe9db8f2b1856506b50baecffff30af
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-19 12:34:00 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,5 @@ ob_start();
 define("DB_DSN", "mysql:host=localhost;dbname=user");
 define("DB_USERNAME", "soowecca");
 define("DB_PASSWORD", "password");
+$conn= new PDO(DB_DSN,DB_USERNAME,DB_PASSWORD);
 ?>
