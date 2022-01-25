f648ef4020c3b02b549e5be968a1883d93a5e9a5
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-25 18:27:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
 <meta name="description" content="Home Page">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="auth.css">
+<link rel="stylesheet" href="/auth.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <div id="container">
 <div class="topnav">
 <div id="mytopnav">
-  <a href="login.php">Sign in</a>
-  <a href="register.php">Sign up</a>
-   <a href="logout.php">Log Out</a>
+  <a href="/login.php">Sign in</a>
+  <a href="/register.php">Sign up</a>
+   <a href="/logout.php">Log Out</a>
  </div>
 <a href="javascript:void(0);" class="icon" onclick="displayIcon()"><i class="fa fa-bars"></i></a>
 </div>
