b58517a86a6c4fb6e3450d4e9fbedc91b2741aa2
bejibay <58639255+bejibay@users.noreply.github.com>
2022-01-25 11:11:24 +0100

    Update activation.php

--- activation.php
+++ activation.php
@@ -9,9 +9,10 @@ $stmt->execute();
 $row = $stmt->fetch();
 if(!$row) $activationError = "Activation URL not found";
 if($row){ 
-$sql = "UPDATE user SET status = 1 where activationurl=:activationurl";
+$sql = "UPDATE user SET status =:status where activationurl=:activationurl";
 $conn->prepare($sql);
 $stmt->bindValue(":activationurl",$_GET['activationurl'],PDO::PARAM_STR);
+$stmt->bindValue(":status",1,PDO::PARAM_INT);
 $stmt->execute();
 $activationSuccess ="Your account is now activated<br>"."Login at "."<a href ='/login.php'>Login</a>";
 }
