f3fd99252f9cb6412a0dcf0337563e2260f0b518
bejibay <58639255+bejibay@users.noreply.github.com>
2022-07-28 19:46:37 +0100

    Update functions.php

--- controller/functions.php
+++ controller/functions.php
@@ -158,3 +158,17 @@ $activationSuccess ="Your account is now activated<br>"."Login at "."<a href ='/
 }
 include "activation.html";
 ?>
+<?php
+// easy use of headers
+ob_start();
+define("DB_DSN", "mysql:host=localhost;dbname=user");
+define("DB_USERNAME", "soowecca");
+define("DB_PASSWORD", "password");
+try{
+$conn= new PDO(DB_DSN,DB_USERNAME,DB_PASSWORD);
+echo "successful connection";
+}
+catch(PDOException $pe){
+echo "something went wrong ".$pe->getMessage():
+}
+?>
