580314af640f82d309ef6b20035c3c80d77472e5
bejibay <58639255+bejibay@users.noreply.github.com>
2022-07-28 19:45:04 +0100

    Update functions.php

--- controller/functions.php
+++ controller/functions.php
@@ -135,3 +135,26 @@ else{$accountError ="account not created";}
 include "/template/register.html";
 ?
 >
+    
+    <?php 
+// include the configuration file
+include "confg.php";
+global $activationError, $activationSuccess;
+if(isset($_GET['activationurl'])){
+$sql = " SELECT* FROM user where activationurl =:activationurl";
+$conn->prepare($sql);
+$stmt->bindValue(":activationurl",$_GET['activationurl'],PDO::PARAM_STR);
+$stmt->execute();
+$row = $stmt->fetch();
+if(!$row) $activationError = "Activation URL not found";
+if($row){ 
+$sql = "UPDATE user SET status =:status where activationurl=:activationurl";
+$conn->prepare($sql);
+$stmt->bindValue(":activationurl",$_GET['activationurl'],PDO::PARAM_STR);
+$stmt->bindValue(":status",1,PDO::PARAM_INT);
+$stmt->execute();
+$activationSuccess ="Your account is now activated<br>"."Login at "."<a href ='/login.php'>Login</a>";
+}
+}
+include "activation.html";
+?>
